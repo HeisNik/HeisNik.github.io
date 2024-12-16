f80598243e29393758b32ee10ae1e625e894db76
Niko Heiskanen <nikoheiska@icloud.com>
2024-12-16 15:22:35 +0200

    Added carousel to app.jsx and added some new css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,15 +9,27 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import SkillsCarousel from './components/SkillsCarousel'
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <div>
       <NavBar/>
       <Hero/>
       <About/>
-      <Skills/>
+      <SkillsCarousel isMobile={isMobile} />
       <Projects/>
       <Contact/>
     </div>
