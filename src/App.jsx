ef149d339f48dcb4d62474ed4dcd953acbdbcf26
Niko Heiskanen <nikoheiska@icloud.com>
2026-01-17 13:36:47 +0200

    some preloading conf

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import './App.css'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import ShaderBackground from './components/ShaderBackground'
+import CyberBackground from './components/CyberBackground'
 import VerticalStackAbout from './components/VerticalStackAbout'
 import CyberGridToolbox from './components/CyberGridToolbox'
+import Preloader from './components/Preloader'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div>
-      <ShaderBackground 
-        backdropBlurAmount="sm" 
-        color="#07eae6ff"
-        className="fixed inset-0 z-0"
-      />
-      <div className="relative z-10 content-wrapper">
-        <NavBar/>
-        <Hero/>
-        <VerticalStackAbout/>
-        <CyberGridToolbox />
-        <Projects/>
-        <Contact/>
-      </div>
+    <div style={{ minHeight: '100vh' }}>
+      <AnimatePresence mode="wait">
+        {isLoading && (
+          <Preloader key="preloader" finishLoading={() => setIsLoading(false)} />
+        )}
+      </AnimatePresence>
+
+      {/* TÄRKEÄÄ: Sisältöä ei edes yritetä piirtää ennen kuin isLoading on false */}
+      {!isLoading && (
+        <motion.div 
+          initial={{ opacity: 0 }} 
+          animate={{ opacity: 1 }} 
+          transition={{ duration: 1.5 }} // Hidas feidaus mustasta
+        >
+          <CyberBackground />
+          <div className="relative z-10 content-wrapper">
+            <NavBar/>
+            <Hero/>
+            <VerticalStackAbout/>
+            <CyberGridToolbox />
+            <Projects/>
+            <Contact/>
+          </div>
+        </motion.div>
+      )}
     </div>
   )
 }
 
-export default App
+export default App;
